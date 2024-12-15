93151d0190546893946559f35802fbb9ce70a2f7
hjw0623 <117283878+hjw0623@users.noreply.github.com>
2024-12-15 20:01:29 +0900

    feat: build-logic 모듈 생성, settings.gradle.kts, convention모듈의 build.gradle.kts 생성

---
+++ build-logic/convention/build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+    `kotlin-dsl`
+}
+
+group = "com.hjw0623.lostark.buildlogic"
+
+dependencies {
+    compileOnly(libs.android.gradlePlugin)
+    compileOnly(libs.android.tools.common)
+    compileOnly(libs.kotlin.gradlePlugin)
+    compileOnly(libs.ksp.gradlePlugin)
+    compileOnly(libs.room.gradlePlugin)
+}
