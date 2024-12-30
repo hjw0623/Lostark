3298c28ad1c45d29580fa7e26ee0ebcd6df5d41f
hjw0623 <117283878+hjw0623@users.noreply.github.com>
2024-12-30 18:16:13 +0900

    feat: 모험 섬 색상 추가

--- core/presentation/designsystem/src/main/java/com/hjw0623/core/presentation/designsystem/Color.kt
+++ core/presentation/designsystem/src/main/java/com/hjw0623/core/presentation/designsystem/Color.kt
@@ -4,8 +4,6 @@ import androidx.compose.ui.graphics.Color
 
 val LostArkBlack = Color(0xFF080707)        // 기본 검은색
 val LostArkBlack30 = Color(0x4D080707)      // 30% 투명 검은색
-val LostArkBlack10 = Color(0x1A080707)      // 10% 투명 검은색
-val LostArkBlack5 = Color(0x0D080707)       // 5% 투명 검은색
 
 val LostArkGray = Color(0xFF87938C)         // 기본 회색
 val LostArkGray40 = Color(0x6687938C)       // 40% 투명 회색
@@ -15,3 +13,7 @@ val LostArkWhite = Color(0xFFFAFAFA)        // 기본 흰색
 
 val LostArkDarkRed = Color(0xFFBB3D3D)      // 진한 빨간색
 val LostArkDarkRed5 = Color(0x0DBB3D3D)     // 5% 투명 진한 빨간색
+
+val LostArkOrange = Color(0xFFFB8C00)
+val LostArkPurple = Color(0xFF5E35B1)
+val LostArkYellow = Color(0xFFFDD835)
