a2384874764eba522d3295a658a9692421a2ed61
hjw0623 <117283878+hjw0623@users.noreply.github.com>
2025-01-02 22:39:27 +0900

    fix: 네트워크Call 유틸함수 변경 및 수정

--- core/data/src/main/java/com/hjw0623/core/data/networking/HttpClientExt.kt
+++ core/data/src/main/java/com/hjw0623/core/data/networking/HttpClientExt.kt
@@ -3,63 +3,13 @@ package com.hjw0623.core.data.networking
 import com.hjw0623.core.data.BuildConfig
 import com.hjw0623.core.domain.util.DataError
 import com.hjw0623.core.domain.util.Result
-import io.ktor.client.HttpClient
 import io.ktor.client.call.body
-import io.ktor.client.request.delete
-import io.ktor.client.request.get
-import io.ktor.client.request.header
-import io.ktor.client.request.parameter
-import io.ktor.client.request.post
-import io.ktor.client.request.setBody
-import io.ktor.client.request.url
 import io.ktor.client.statement.HttpResponse
 import io.ktor.util.network.UnresolvedAddressException
 import io.ktor.utils.io.CancellationException
 import kotlinx.serialization.SerializationException
 
-suspend inline fun <reified Response : Any> HttpClient.get(
-    route: String,
-    queryParameters: Map<String, Any?> = mapOf(),
-    headers: Map<String, String> = mapOf()
-): Result<Response, DataError.Network> {
-    return safeCall {
-        get {
-            url(constructRoute(route))
-            queryParameters.forEach { (key, value) ->
-                parameter(key, value)
-            }
-            headers.forEach { (key, value) ->
-                header(key, value)
-            }
-        }
-    }
-}
 
-suspend inline fun <reified Response: Any> HttpClient.delete(
-    route: String,
-    queryParameters: Map<String, Any?> = mapOf()
-): Result<Response, DataError.Network> {
-    return safeCall {
-        delete {
-            url(constructRoute(route))
-            queryParameters.forEach { (key, value) ->
-                parameter(key,value)
-            }
-        }
-    }
-}
-
-suspend inline fun <reified Request, reified Response: Any> HttpClient.post(
-    route: String,
-    body: Request
-): Result<Response, DataError.Network> {
-    return safeCall {
-        post {
-            url(constructRoute(route))
-            setBody(body)
-        }
-    }
-}
 suspend inline fun <reified T> safeCall(execute: () -> HttpResponse): Result<T, DataError.Network> {
     val response = try {
         execute()
@@ -98,3 +48,4 @@ fun constructRoute(route: String): String {
         else -> BuildConfig.BASE_URL + "/$route"
     }
 }
+
--- core/domain/src/main/java/com/hjw0623/core/domain/util/Result.kt
+++ core/domain/src/main/java/com/hjw0623/core/domain/util/Result.kt
@@ -2,7 +2,7 @@ package com.hjw0623.core.domain.util
 
 sealed interface Result<out D, out E : Error> {
     data class Success<out D>(val data: D) : Result<D, Nothing>
-    data class Error<out E : com.hjw0623.core.domain.util.Error>(val error: E) : Result<Nothing, E>
+    data class Error<out E : DomainError>(val error: E) : Result<Nothing, E>
 }
 
 inline fun <T, E : Error, R> Result<T, E>.map(map: (T) -> R): Result<R, E> {
@@ -16,9 +16,25 @@ fun  <T, E: Error> Result<T, E>.asEmptyDataResult(): EmptyDataResult<E> {
     return map {  }
 }
 
+inline fun <T, E: Error> Result<T, E>.onSuccess(action: (T) -> Unit): Result<T, E> {
+    return when(this) {
+        is Result.Error -> this
+        is Result.Success -> {
+            action(data)
+            this
+        }
+    }
+}
+
+inline fun <T, E: Error> Result<T, E>.onError(action: (E) -> Unit): Result<T, E> {
+    return when(this) {
+        is Result.Error -> {
+            action(error)
+            this
+        }
+        is Result.Success -> this
+    }
+}
 typealias EmptyDataResult<E> = Result<Unit, E>
-
-
-
-
+typealias DomainError = Error
 
