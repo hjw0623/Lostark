430de88356e8bf32a8a7e332745fc99e0e452115
hjw0623 <117283878+hjw0623@users.noreply.github.com>
2025-01-02 22:40:23 +0900

    fix: 유틸함수 변경으로 인한 api콜 변경

--- events/data/src/main/java/com/hjw0623/events/data/EventsRepositoryImpl.kt
+++ events/data/src/main/java/com/hjw0623/events/data/EventsRepositoryImpl.kt
@@ -1,68 +1,55 @@
 package com.hjw0623.events.data
 
-import com.hjw0623.core.data.networking.events.model.EventSerializable
-import com.hjw0623.core.data.networking.events.toDomain
-import com.hjw0623.core.data.networking.islands.model.IslandsSerializable
-import com.hjw0623.core.data.networking.islands.toDomain
-import com.hjw0623.core.data.networking.get
-import com.hjw0623.core.data.networking.notices.model.NoticeSerializable
-import com.hjw0623.core.data.networking.notices.toDomain
-import com.hjw0623.core.domain.events.model.Event
-import com.hjw0623.core.domain.islands.model.Islands
-import com.hjw0623.core.domain.notices.model.Notice
+import com.hjw0623.core.data.networking.constructRoute
+import com.hjw0623.core.data.networking.safeCall
 import com.hjw0623.core.domain.util.DataError
 import com.hjw0623.core.domain.util.Result
 import com.hjw0623.core.domain.util.map
+import com.hjw0623.events.data.events.model.EventSerializable
+import com.hjw0623.events.data.events.toDomain
+import com.hjw0623.events.data.islands.model.IslandsSerializable
+import com.hjw0623.events.data.islands.toDomain
+import com.hjw0623.events.data.notices.model.NoticeSerializable
+import com.hjw0623.events.data.notices.toDomain
+import com.hjw0623.events.domain.Event
 import com.hjw0623.events.domain.EventsRepository
+import com.hjw0623.events.domain.Islands
+import com.hjw0623.events.domain.Notice
 import io.ktor.client.HttpClient
+import io.ktor.client.request.get
 
 
 class EventsRepositoryImpl(
     private val httpClient: HttpClient
 ) : EventsRepository {
-
-    private val apiKey = "bearer " + BuildConfig.API_KEY
-    private val headerMap = mapOf(
-        "Authorization" to apiKey,
-        "Accept" to "application/json"
-    )
-
     override suspend fun getIslands(): Result<List<Islands>, DataError.Network> {
-        val response: Result<ArrayList<IslandsSerializable>, DataError.Network> = httpClient.get(
-            route = "/gamecontents/calendar",
-            headers = headerMap
-        )
-        val result = response.map { list ->
-            list.map { it.toDomain() }
-                .filter { it.categoryName == "모험 섬" }
+        return safeCall<List<IslandsSerializable>> {
+            httpClient.get(
+                urlString = constructRoute("/gamecontents/calendar")
+            )
+        }.map { response ->
+            response.map { it.toDomain() }
         }
-        return result
     }
 
     override suspend fun getEvents(): Result<List<Event>, DataError.Network> {
-        val response: Result<List<EventSerializable>, DataError.Network> = httpClient.get(
-            route = "/news/events",
-            headers = headerMap
-        )
-        val result = response.map { list ->
-            list.map {
-                it.toDomain()
-            }
+        return safeCall<List<EventSerializable>> {
+            httpClient.get(
+                urlString = constructRoute("/news/events")
+            )
+        }.map { response ->
+            response.map { it.toDomain()}
         }
-        return result
     }
 
     override suspend fun getNotices(): Result<List<Notice>, DataError.Network> {
-        val response: Result<List<NoticeSerializable>, DataError.Network> = httpClient.get(
-            route = "/news/notices",
-            headers = headerMap
-        )
-        val result = response.map { list ->
-            list.map {
-                it.toDomain()
-            }
+        return safeCall<List<NoticeSerializable>> {
+            httpClient.get(
+                urlString = constructRoute("/news/notices")
+            )
+        }.map { response ->
+            response.map { it.toDomain()}
         }
-        return result
     }
 }
 
--- events/domain/src/main/java/com/hjw0623/events/domain/EventsRepository.kt
+++ events/domain/src/main/java/com/hjw0623/events/domain/EventsRepository.kt
@@ -1,8 +1,5 @@
 package com.hjw0623.events.domain
 
-import com.hjw0623.core.domain.events.model.Event
-import com.hjw0623.core.domain.islands.model.Islands
-import com.hjw0623.core.domain.notices.model.Notice
 import com.hjw0623.core.domain.util.DataError
 import com.hjw0623.core.domain.util.Result
 
