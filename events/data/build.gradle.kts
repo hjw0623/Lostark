0588c5557abcc74c84ab1c02b9dee5103f84a13f
hjw0623 <117283878+hjw0623@users.noreply.github.com>
2025-01-07 14:44:29 +0900

    feat: koin라이브러리 추가

--- events/data/build.gradle.kts
+++ events/data/build.gradle.kts
@@ -8,6 +8,8 @@ android {
 }
 
 dependencies {
+    implementation(libs.bundles.koin)
+
     implementation(projects.events.domain)
     implementation(projects.core.domain)
     implementation(projects.core.data)
