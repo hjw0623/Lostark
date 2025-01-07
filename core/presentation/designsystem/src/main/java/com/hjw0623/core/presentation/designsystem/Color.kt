bfae932ec379903d7f32f75289283b431894db46
hjw0623 <117283878+hjw0623@users.noreply.github.com>
2025-01-07 14:43:43 +0900

    fix: 색상 및 theme 변경

--- core/presentation/designsystem/src/main/java/com/hjw0623/core/presentation/designsystem/Color.kt
+++ core/presentation/designsystem/src/main/java/com/hjw0623/core/presentation/designsystem/Color.kt
@@ -2,17 +2,17 @@ package com.hjw0623.core.presentation.designsystem
 
 import androidx.compose.ui.graphics.Color
 
-val LostArkBlack = Color(0xFF080707)        // 기본 검은색
-val LostArkBlack30 = Color(0x4D080707)      // 30% 투명 검은색
-
+val LostArkBlue = Color(0xFF2250FB)
+val LostArkLightBlue = Color(0xFFC4CAF8)
+val LostArkWhite = Color(0xFFFAFAFA)
+val LostArkDarkRed = Color(0xFFBB3D3D)
 val LostArkGray = Color(0xFF87938C)         // 기본 회색
-val LostArkGray40 = Color(0x6687938C)       // 40% 투명 회색
-val LostArkDarkGray = Color(0xFF232624)     // 어두운 회색
+val LostArkBlack = Color(0xFF000000)     // 어두운 회색
+val LostArkGreen = Color(0xFF48C65B)
 
-val LostArkWhite = Color(0xFFFAFAFA)        // 기본 흰색
 
-val LostArkDarkRed = Color(0xFFBB3D3D)      // 진한 빨간색
-val LostArkDarkRed5 = Color(0x0DBB3D3D)     // 5% 투명 진한 빨간색
+
+
 
 val LostArkOrange = Color(0xFFFB8C00)
 val LostArkPurple = Color(0xFF5E35B1)
