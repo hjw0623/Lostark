a0a652814f8bc4e0e89fe94cbd58ab91fa146518
hjw0623 <117283878+hjw0623@users.noreply.github.com>
2025-02-28 01:18:06 +0900

    feat: di 추가

--- character/data/src/main/java/com/hjw0623/data/di/CharacterDataModule.kt
+++ character/data/src/main/java/com/hjw0623/data/di/CharacterDataModule.kt
@@ -1,11 +1,15 @@
 package com.hjw0623.data.di
 
 import com.hjw0623.character.domain.CharacterRepository
+import com.hjw0623.character.domain.CharacterSearchPreferences
 import com.hjw0623.data.CharacterRepositoryImpl
+import com.hjw0623.data.CharacterSearchPreferencesImpl
+import org.koin.android.ext.koin.androidContext
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.bind
 import org.koin.dsl.module
 
 val characterDataModule = module {
     singleOf(::CharacterRepositoryImpl).bind<CharacterRepository>()
+    single<CharacterSearchPreferences> { CharacterSearchPreferencesImpl(androidContext()) }
 }
