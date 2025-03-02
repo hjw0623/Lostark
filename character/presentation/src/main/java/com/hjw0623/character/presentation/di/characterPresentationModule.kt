6775306d4b472805b6dc177a6f5d4785aecc5d8c
hjw0623 <117283878+hjw0623@users.noreply.github.com>
2025-03-03 00:16:54 +0900

    feat: di 추가

--- character/presentation/src/main/java/com/hjw0623/character/presentation/di/characterPresentationModule.kt
+++ character/presentation/src/main/java/com/hjw0623/character/presentation/di/characterPresentationModule.kt
@@ -1,5 +1,6 @@
 package com.hjw0623.character.presentation.di
 
+import com.hjw0623.character.presentation.character_add.CharacterAddViewModel
 import com.hjw0623.character.presentation.character_manager.CharacterManagerViewModel
 import com.hjw0623.character.presentation.character_overview.CharacterOverviewViewModel
 import com.hjw0623.character.presentation.character_search.CharacterSearchViewModel
@@ -10,4 +11,5 @@ val characterPresentationModule = module {
     viewModelOf(::CharacterOverviewViewModel)
     viewModelOf(::CharacterManagerViewModel)
     viewModelOf(::CharacterSearchViewModel)
+    viewModelOf(::CharacterAddViewModel)
 }
