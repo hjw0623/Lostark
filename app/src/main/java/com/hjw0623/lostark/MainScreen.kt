2dfb545a9de9601c3764127b07c25212acfdd84d
hjw0623 <117283878+hjw0623@users.noreply.github.com>
2025-03-16 16:09:15 +0900

    feat: manager, search, event 화면에서만 네브바를 보여주는 기능 추가

--- app/src/main/java/com/hjw0623/lostark/MainScreen.kt
+++ app/src/main/java/com/hjw0623/lostark/MainScreen.kt
@@ -4,7 +4,9 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 
 
@@ -12,11 +14,24 @@ import androidx.navigation.compose.rememberNavController
 fun MainScreen() {
     val navController = rememberNavController()
 
+    val bottomBarScreens = listOf(
+        BottomNavItem.CHARACTER_MANAGER.route,
+        BottomNavItem.CHARACTER_SEARCH.route,
+        BottomNavItem.EVENT_SCREEN.route
+    )
+
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentRoute = navBackStackEntry?.destination?.route
+
     Scaffold(
-        bottomBar = { BottomNavigationBar(navController) }
+        bottomBar = {
+            if (currentRoute in bottomBarScreens) {
+                BottomNavigationBar(navController)
+            }
+        }
     ) { paddingValues ->
         Box(modifier = Modifier.padding(paddingValues)) {
-            NavigationRoot(navController)
+            NavigationRoot(navController = navController)
         }
     }
 }
