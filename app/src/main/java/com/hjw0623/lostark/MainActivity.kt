cbc98cd2d6f08504de29b85fb4f0aff85fa15b29
hjw0623 <117283878+hjw0623@users.noreply.github.com>
2025-04-04 01:31:14 +0900

    feat: splashScreen, appLogo 추가

--- app/src/main/java/com/hjw0623/lostark/MainActivity.kt
+++ app/src/main/java/com/hjw0623/lostark/MainActivity.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.hjw0623.core.presentation.designsystem.LostarkTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen().apply { setKeepOnScreenCondition { false } }
         enableEdgeToEdge()
         setContent {
             LostarkTheme {
